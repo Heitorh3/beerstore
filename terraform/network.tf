a8ce8f7ef0ef0351abafb43cf14c146e367b6aee
Heitor A Neto <heitorh3@gmail.com>
2018-12-05 16:43:50 -0200

    Provisionando as subnets privadas

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,7 +1,20 @@
 resource "aws_vpc" "main" {
-  cidr_block = "192.168.00/16"
+  cidr_block = "192.168.0.0/16"
 
   tags {
     Name = 'hibicode'
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  count = 3
+
+  cidr_block = "${cidrsubnet(aws_vpc.main.cidr_block,8 ,count.index + 10 )}"
+
+  vpc_id = "${aws_vpc.main.id}"
+  availability_zone = "${var.availability_zones[count.index]}"
+
+  tags {
+    Name="hibicode_private_subnet_${count.index}"
   }
 }
