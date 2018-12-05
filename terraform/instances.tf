1cac8543b366937a6f0dd00e9740bfcc73cee719
Heitor Neto <heitorh3@gmail.com>
2018-12-05 18:16:19 -0200

    Provisionando as subnets privadas Provisionando máquinas EC2

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,3 +1,7 @@
+resource "aws_key_pair" "key_par" {
+  public_key = "${file("key/beerstore_key.pub")}"
+}
+
 resource "aws_instance" "instances" {
   count = 3
 
@@ -6,7 +10,13 @@ resource "aws_instance" "instances" {
 
   subnet_id = "${element(aws_subnet.public_subnet.*.id, count.index )}"
 
+  key_name = "${aws_key_pair.key_par.key_name}"
+
   tags {
     Name = "hibicode_instances"
   }
+}
+
+output "public_ips" {
+  value = "${join(", ", aws_instance.instances.*.public_ip)}"
 }
