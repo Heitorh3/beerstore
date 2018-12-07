c7bd3a9be81d6e569de423d2f6c9cb5246511006
Heitor A Neto <heitorh3@gmail.com>
2018-12-07 17:01:47 -0200

    Inicializando o líder do Docker Swarm

--- terraform/security.tf
+++ terraform/security.tf
@@ -71,3 +71,24 @@ resource "aws_security_group" "cluster_communication" {
   }
 
 }
+
+resource "aws_security_group" "allow_app" {
+  vpc_id = "${aws_vpc.main.id}"
+  name = "hibicode_allow_app"
+
+  ingress {
+    from_port = 9000
+    to_port = 9000
+    protocol = "tcp"
+    cidr_blocks = ["${var.my_public_ip}"]
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = [
+      "${flatten(
+            chunklist(aws_subnet.public_subnet.*.cidr_block, 1))}"]
+  }
+}
