fad63924611db65099c6f789ea0fe392026bcd93
Heitor A Neto <heitorh3@gmail.com>
2018-12-07 15:05:38 -0200

    Autorizando outbound via Security Group

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "allow_ssh" {
   vpc_id = "${aws_vpc.main.id}"
-  name = "allow_ssh"
+  name = "hibicode_allow_ssh"
 
   ingress {
     from_port = 22
@@ -20,6 +20,20 @@ resource "aws_security_group" "security_group_database" {
     protocol = "tcp"
     to_port = 5432
     self = true
+  }
+
+}
+
+resource "aws_security_group" "allow_outbound" {
+
+  vpc_id = "${aws_vpc.main.id}"
+  name = "hibicode_allow_outbound"
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 }
